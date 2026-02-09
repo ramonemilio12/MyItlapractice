4eed5d8aebdf68efce50268263b82481c10bace7
ramonemilio12 <r.emiliojimenez@gmail.com>
2026-02-09 18:45:37 -0400

    Adding my second submission

---
+++ TASK2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool valido = false;
+
+while (!valido)
+{
+    try
+    {
+        Console.WriteLine("Ingrese un número:");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"El número {numero} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numero} es impar.");
+        }
+
+        valido = true; 
+    }
+    catch
+    {
+        Console.WriteLine("Error: debe ingresar un número válido.");
+    }
+}
+
+
